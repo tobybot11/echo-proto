640ff2ae781c81410343c195033177bae3416975
Tobias Ford <toby.ford@pobox.com>
2018-01-29 14:52:32 -0500

    added decoder and encoder to the LineCodec - cargo check runs aok

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,39 @@ use tokio_io::codec::{Encoder, Decoder};
 
 pub struct LineCodec;
 
+impl Decoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<String>> {
+        if let Some(i) = buf.iter().position(|&b| b == b'\n') {
+            // remove the serialized frame from the buffer
+            let line = buf.split_to(i);
+
+            // Also remove the '\n'
+            buf.split_to(1);
+
+            match str::from_utf8(&line) {
+                Ok(s) => Ok(Some(s.to_string())),
+                Err(_) => Err(io::Error::new(io::ErrorKind::Other, "invalid UTF-8")),
+            }
+
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl Encoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn encode(&mut self, msg: String, buf: &mut BytesMut) -> io::Result<()> {
+        buf.extend(msg.as_bytes());
+        buf.extend(b"\n");
+        Ok(())
+    }
+}
 
 fn main() {
     println!("Hello, world!");
