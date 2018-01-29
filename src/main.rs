64117441080083461b51d05f3e5e1108db9b9eb2
Tobias Ford <toby.ford@pobox.com>
2018-01-29 14:41:34 -0500

    added correct externs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate bytes;
+extern crate futures;
+extern crate tokio_io;
+extern crate tokio_proto;
+extern crate tokio_service;
+
+
+// a server in tokio_proto is made up of three distinct parts
+// a transport ...   implemented using the framed helper
+// a protocol specification ..   codex, basic info, multiplexed?, streaming?
+// a service .. how to produce a response give a request .. async function
+
 fn main() {
     println!("Hello, world!");
 }
